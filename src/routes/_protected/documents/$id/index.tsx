7489423e0c5eaf07fb21d1a4253829d2f51c4c81
Woollywood <115595112+Woollywood@users.noreply.github.com>
2025-03-16 19:20:01 +0300

    feat: navigate when access denied document id

--- src/routes/_protected/documents/$id/index.tsx
+++ src/routes/_protected/documents/$id/index.tsx
@@ -1,15 +1,24 @@
 import { useGetDocumentById } from '@/api/hooks/queries/documents';
 import { Editor } from '@/components/shared/editor';
 import { Navbar } from '@/components/shared/navbars/document';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { AxiosError } from 'axios';
+import { useEffect } from 'react';
 
 export const Route = createFileRoute('/_protected/documents/$id/')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const navigate = useNavigate();
 	const { id } = Route.useParams();
-	useGetDocumentById(id);
+	const { error } = useGetDocumentById(id);
+
+	useEffect(() => {
+		if (error instanceof AxiosError) {
+			navigate({ to: '/' });
+		}
+	}, [error, navigate]);
 
 	return (
 		<div className='grid h-full min-h-screen grid-rows-[auto_1fr] gap-y-8'>
